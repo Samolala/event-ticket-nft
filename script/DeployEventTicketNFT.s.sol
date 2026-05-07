1652f08e05975fc7bee1045649474f9961fd8930
Samuel Moses <mosessamuel0470@gmail.com>
2026-05-07 13:19:12 +0100

    Implement deployment script for EventTicketNFT

--- script/DeployEventTicketNFT.s.sol
+++ script/DeployEventTicketNFT.s.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import {Script, console} from "forge-std/Script.sol";
+import {EventTicketNFT} from "../src/EventTicketNFT.sol";
+
+contract DeployEventTicketNFT is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+        
+        console.log("Deploying with account:", deployer);
+        console.log("Account balance:", deployer.balance);
+        
+        vm.startBroadcast(deployerPrivateKey);
+        
+        EventTicketNFT ticketNFT = new EventTicketNFT(
+            "EventTicketNFT",
+            "TICKET",
+            "https://api.example.com/metadata/",
+            deployer
+        );
+        
+        vm.stopBroadcast();
+        
+        console.log("EventTicketNFT deployed to:", address(ticketNFT));
+    }
+}
